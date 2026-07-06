3f2141144cf48a5f169fbf14beedc8198aab5674
Vaibhav Kushwaha <vaibhav.kushwaha215@gmail.com>
2026-07-06 10:43:54 +0530

    Initialize

---
+++ src/Calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        //Initialization
+        double num1,num2,num3;
+        //Calculator
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("""
+
+        ----------Welcome to Calculator----------
+        Please enter what would you like to do?
+        1. Addition
+        2. Subtraction
+        3. Multiplication
+        4. Division
+         5.Remainder
+        """);
+        int meth = scanner.nextInt();
+        switch (meth) {
+            case 1, 2, 3, 4, 5 -> {
+                System.out.println("Please enter the first number");
+                num1 = scanner.nextDouble();
+
+                System.out.println("Please enter the second number");
+                num2 = scanner.nextDouble();
+
+                num3 = switch (meth) {
+                    case 1 -> num1 + num2;
+                    case 2 -> num1 - num2;
+                    case 3 -> num1 * num2;
+                    case 4 -> num1 / num2;
+                    case 5 -> num1 % num2;
+                    default -> throw new AssertionError(); // unreachable
+                };
+                System.out.printf("The output is %.2f%n", num3);
+            }
+
+            default -> System.out.println("Invalid Method");
+        }
+            /*  This can be used there as well
+            System.out.printf(
+                flag ? "The output is %.2f%n" : "Invalid Method%n",
+                num3
+            );
+             */
+    }
+}
