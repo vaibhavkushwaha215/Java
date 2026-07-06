a7e280ea2c17ef89c97e6288d67a12cca13a54c7
Vaibhav Kushwaha <vaibhav.kushwaha215@gmail.com>
2026-07-06 12:02:34 +0530

    While loop

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,29 +14,38 @@ public class Calculator {
         3. Multiplication
         4. Division
          5.Remainder
+         6. Exit
         """);
-        int meth = scanner.nextInt();
-        switch (meth) {
-            case 1, 2, 3, 4, 5 -> {
-                System.out.println("Please enter the first number");
-                num1 = scanner.nextDouble();
 
-                System.out.println("Please enter the second number");
-                num2 = scanner.nextDouble();
+        boolean flag = true;
+        while(flag) {
+            int meth = scanner.nextInt();
+            if (meth == 6){
+                flag = false;
+            }
+            switch (meth) {
+                case 1, 2, 3, 4, 5 , 6-> {
+                    System.out.println("Please enter the first number");
+                    num1 = scanner.nextDouble();
 
-                num3 = switch (meth) {
-                    case 1 -> num1 + num2;
-                    case 2 -> num1 - num2;
-                    case 3 -> num1 * num2;
-                    case 4 -> num1 / num2;
-                    case 5 -> num1 % num2;
-                    default -> throw new AssertionError(); // unreachable
-                };
-                System.out.printf("The output is %.2f%n", num3);
+                    System.out.println("Please enter the second number");
+                    num2 = scanner.nextDouble();
+
+                    num3 = switch (meth) {
+                        case 1 -> num1 + num2;
+                        case 2 -> num1 - num2;
+                        case 3 -> num1 * num2;
+                        case 4 -> num1 / num2;
+                        case 5 -> num1 % num2;
+                        case 6 -> 0;
+                        default -> throw new AssertionError(); // unreachable
+                    };
+
+                    System.out.printf("The output is %.2f%n", num3);
             }
 
             default -> System.out.println("Invalid Method");
-        }
+        }}
             /*  This can be used there as well
             System.out.printf(
                 flag ? "The output is %.2f%n" : "Invalid Method%n",
