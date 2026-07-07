8dcf5e546411a5c05bf29015788bd0207f8d8eeb
Vaibhav Kushwaha <vaibhav.kushwaha215@gmail.com>
2026-07-07 23:15:35 +0530

    Slight Modifications

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Calculator {
-    public static void main(String[] args) {
-        //Initialization
-        double num1,num2,num3;
-        //Calculator
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("""
+	public static void main(String[] args) {
+		//Initialization
+		double num1,num2,num3;
+		//Calculator
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("""
 
         ----------Welcome to Calculator----------
         Please enter what would you like to do?
@@ -16,41 +16,46 @@ public class Calculator {
          5.Remainder
          6. Exit
         """);
-
-        boolean flag = true;
-        while(flag) {
-            int meth = scanner.nextInt();
-            if (meth == 6){
-                flag = false;
-            }
-            switch (meth) {
-                case 1, 2, 3, 4, 5 , 6-> {
-                    System.out.println("Please enter the first number");
-                    num1 = scanner.nextDouble();
-
-                    System.out.println("Please enter the second number");
-                    num2 = scanner.nextDouble();
-
-                    num3 = switch (meth) {
-                        case 1 -> num1 + num2;
-                        case 2 -> num1 - num2;
-                        case 3 -> num1 * num2;
-                        case 4 -> num1 / num2;
-                        case 5 -> num1 % num2;
-                        case 6 -> 0;
-                        default -> throw new AssertionError(); // unreachable
-                    };
-
-                    System.out.printf("The output is %.2f%n", num3);
-            }
-
-            default -> System.out.println("Invalid Method");
-        }}
+		boolean flag = true;
+		while(flag) {
+			int meth = scanner.nextInt();
+					switch (meth){
+						case 1 -> System.out.println(" You have Chosen Addition");
+						case 2 -> System.out.println(" You have Chosen Subtraction");
+						case 3 -> System.out.println(" You have Chosen Multiplication");
+						case 4 -> System.out.println(" You have Chosen Division");
+						case 5 -> System.out.println(" You have Chosen Remainder");
+						case 6 -> {
+							System.out.println("Exiting...");
+							flag=false;
+							continue;
+						}
+						default -> {
+							System.out.println("Invalid Input!");
+							continue;
+						}
+					}
+					switch (meth){
+						case 1,2,3,4,5 ->{
+							System.out.println("Please enter the first number");
+							num1 = scanner.nextDouble();
+							System.out.println("Please enter the second number");
+							num2 = scanner.nextDouble();
+							num3 = switch (meth) {
+								case 1 -> num1 + num2;
+								case 2 -> num1 - num2;
+								case 3 -> num1 * num2;
+								case 4 -> num1 / num2;
+								case 5 -> num1 % num2;
+								default -> throw new AssertionError(); // unreachable
+							};
+							System.out.printf("The output is %.2f%n", num3);}}
+				}
+			}
             /*  This can be used there as well
             System.out.printf(
                 flag ? "The output is %.2f%n" : "Invalid Method%n",
                 num3
             );
-             */
-    }
+            */
 }
